5f6c74537d80879e1e32cac6c4cbb928c0ec6fd0
root <root@DESKTOP-VJF7JKN.localdomain>
2022-06-26 00:36:42 -0400

    edited script file to reflect the use of services to deploy the site

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,11 +3,12 @@
 #This script redeploys my portfolio site after any changes have been made to the site
 #on the GitHub repo. It automates all the manual steps previously taken to do so
 
-tmux kill-server
+#tmux kill-server #no longer needed as deployement process is now using services
 cd /root/project-placid-pachyderm
 git fetch
 git reset origin/main --hard
 
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new -ds flask-placidportfolio 'flask run --host=0.0.0.0;'
+#tmux new -ds flask-placidportfolio 'flask run --host=0.0.0.0;' #no longer needed as deployement is through services
+systemctl restart myportfolio
